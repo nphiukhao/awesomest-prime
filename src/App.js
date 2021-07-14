2dbf544640f87c0da650b07c960f58e8ba31609f
chipewpew <cphiukhao@gmail.com>
2021-07-13 23:00:19 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
 import logo from './logo.svg';
+import React, { useState } from 'react';
+import { Route } from 'react-router-dom';
+
+
+import About from './Components/About'
+
 import './App.css';
 
 function App() {
+
+const [title, setTitle] = useState("");
+
+  function getRecipe() {
+    console.log('hello')
+        return fetch(`https://ghibliapi.herokuapp.com/films/`)
+        .then(res =>             
+            (!res.ok)
+                ? res.json().then(e => Promise.reject(e))
+                : res.json()
+        )}
+
+  function buildItems() {
+    getRecipe().then(response => setTitle(response[Math.floor(Math.random()*response.length)].title))
+    
+  }      
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <ul className="nav">
+            <li className="nav-item">
+              <a className="nav-link active" aria-current="page" href="/about">About</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link active" aria-current="page" href="/">Home</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Link</a>
+            </li>
+
+          </ul>
+        </header>
+          <h1>Movie: {title}</h1>
+          <button type="button" className="btn btn-primary" onClick={e => buildItems()}>give me a movie</button>
+          <Route path='/about' component={About} />
+      </div>
+
   );
 }
 
